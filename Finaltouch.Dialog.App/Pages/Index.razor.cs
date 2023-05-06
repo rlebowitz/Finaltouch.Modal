0788c50d5f5097eb7a939a34bbeec38c5b3625c5
Robert Lebowitz <lebowitz@finaltouch.com>
2023-05-06 14:44:40 -0500

    Add project files.

---
+++ Finaltouch.Dialog.App/Pages/Index.razor.cs
@@ -0,0 +1,14 @@
+﻿using Finaltouch.Modal.App.Shared;
+
+namespace Finaltouch.Modal.App.Pages
+{
+    public partial class Index
+    {
+        private DialogTemplate NoHeader { get; set; } = default!;
+        private DialogTemplate NoFooter { get; set; } = default!;
+        private DialogTemplate WithHeader { get; set; } = default!;
+        private DialogTemplate Scrollable { get; set; } = default!;
+        private DialogTemplate Zoom { get; set; } = default!;
+     
+    }
+}
---
+++ Finaltouch.Dialog.App/Shared/DialogTemplate.razor.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Finaltouch.Modal.App.Shared
+{
+    /// <summary>
+    /// A Blazor Modal Template Component
+    /// </summary>
+    /// <remarks>
+    /// https://stackoverflow.com/questions/59256798/how-to-use-bootstrap-modal-in-blazor-client-app
+    /// </remarks>
+    public partial class DialogTemplate
+    {
+        [Parameter]
+        public RenderFragment? Header { get; set; }
+        [Parameter]
+        public RenderFragment? Body { get; set; }
+        [Parameter]
+        public RenderFragment? Footer { get; set; }
+        [Parameter]
+        public string? Title { get; set; }
+        [Parameter]
+        public bool UseZoom { get; set; }
+        [Parameter]
+        public bool Scrollable { get; set; }
+        public bool Centered { get; set; } = true;
+        private string ModalDisplay { get; set; } = "none";
+        private string ModalClass { get; set; } = string.Empty;
+        private string ModalZoom => UseZoom ? "modal-zoom" : string.Empty;
+        private string ModalScroll => Scrollable ? "modal-dialog-scrollable" : string.Empty;
+        private string ModalCentered => Centered ? "modal-dialog-centered " : string.Empty;
+        private Dictionary<string, object> AriaAttributes
+        {
+            get
+            {
+                var attributes = new Dictionary<string, object>();
+                if ("block".Equals(ModalDisplay))
+                {
+                    attributes.Clear();
+                    attributes.Add("aria-modal", "true");
+                    attributes.Add("role", "dialog");
+                }
+                else
+                {
+                    attributes.Clear();
+                    attributes.Add("aria-hidden", "true");
+                }
+                return attributes;
+            }
+        }
+
+        public async Task Open()
+        {
+            ModalDisplay = "block";
+            await Task.Delay(200);
+            ModalClass = "show";
+        }
+
+        public async Task Close()
+        {
+            ModalClass = string.Empty;
+            await Task.Delay(200);
+            ModalDisplay = "none";
+        }
+
+    }
+}
